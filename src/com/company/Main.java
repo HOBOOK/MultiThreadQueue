a51879d1c07689d898eae2f66f9f27e6856d46ec
박경호 <pkh879@gmail.com>
2020-06-10 20:44:04 +0900

    멀티 쓰레드큐 프로젝트 생성

---
+++ src/com/company/Main.java
@@ -0,0 +1,108 @@
+package com.company;
+
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HoQueue hoQueue = new HoQueue(10);
+
+        Publisher publisher = new Publisher(hoQueue);
+        Publisher publisher1 = new Publisher(hoQueue);
+        Consumer consumer = new Consumer(hoQueue);
+
+        publisher.start();
+        consumer.start();
+        publisher1.start();
+
+    }
+}
+
+
+class HoQueue{
+    Queue<String> queue;
+    int maxSize;
+    public HoQueue(int maxSize){
+        queue = new LinkedList<>();
+        this.maxSize = maxSize;
+    }
+    public synchronized void enqueue(String str){
+        if(!isPushAble()){
+            try{
+                wait();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        notify();
+        queue.add(str);
+    }
+    public synchronized String dequeue(){
+        if(!isPollAble()){
+            try{
+                wait();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        notify();
+        return queue.poll();
+    }
+    public boolean isPushAble(){
+        return queue.size() < maxSize;
+    }
+
+    public boolean isPollAble(){
+        return queue.size() > 0;
+    }
+
+    public boolean isEmpty(){
+        return queue.isEmpty();
+    }
+}
+
+
+class Publisher extends Thread{
+    List<String> pockets;
+    HoQueue hoQueue;
+    int index;
+    public Publisher(HoQueue hoQueue){
+        this.pockets = new ArrayList<>();
+        pockets.add("가");
+        pockets.add("나");
+        pockets.add("다");
+        pockets.add("라");
+        pockets.add("바");
+        index = pockets.size()-1;
+        this.hoQueue = hoQueue;
+    }
+
+    @Override
+    public void run() {
+        while(index>=0){
+            hoQueue.enqueue(pockets.get(index));
+            System.out.println("큐 삽입 : " + pockets.get(index));
+            index--;
+        }
+    }
+}
+
+class Consumer extends Thread{
+    public boolean isRun = true;
+    List<String> pockets;
+    HoQueue hoQueue;
+    public Consumer(HoQueue hoQueue){
+        this.pockets = new ArrayList<>();
+        this.hoQueue = hoQueue;
+    }
+
+    @Override
+    public void run() {
+        while (isRun){
+            pockets.add(hoQueue.dequeue());
+            System.out.println("큐 제거 : " + pockets.get(pockets.size()-1));
+        }
+    }
+}
+
